87ef1411a737a3ea4934ebd671ea3e721948a1b3
dhanuhangout <hangouttest10@gmail.com>
2016-11-10 12:44:27 -0800

    template with different arg types examples.

--- func_template.cpp
+++ func_template.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-/* Function template example:
+/* Function template example 1:
  * This example demonstrates the function overloading example in simplify way,
  * using function template.
  * If the function overload has same functionality, then we go with function
@@ -13,11 +13,32 @@ T arith_add(T a, T b) {
   return result;
 }
 
+/* Function template example 2:
+ * This example demonstrates the function with difference in arguments type. */
+template <class T, class U>
+T arith_equal(T a, U b) {
+  return (a == b);
+}
+
+/* Function template example 3:
+ * This example demonstrates the function with Non type arguments. */
+template <class T, int N>
+T arith_mul(T a) {
+  return (a * N);
+}
+
 int main() {
   int x=5, y=2;
   double n=5.0, m=2.0;
-  /* Demonstrating function template. */
-  cout << arith_add(x,y) << endl;
-  cout << arith_add(n,m) << endl;
+  /* Demonstrating example 1. */
+  cout << arith_add(x, y) << endl;
+  cout << arith_add(n, m) << endl;
+  /* Demonstrating example 2. */
+  if (arith_equal(y, m))
+    cout << "y and m are equal" << endl;
+  else
+    cout << "y and m are not equal" << endl;
+  /* Demonstrating example 3. */
+  cout << arith_mul<int, 2>(x) << endl;
   return 0;
 }
